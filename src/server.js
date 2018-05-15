3653ac64685b6e655cedfc5b47a116b84eb42972
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2018-05-15 01:08:50 -0400

    switching to use the express framework

--- src/server.js
+++ src/server.js
@@ -1,18 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(JSON.stringify({
-    "data": {"hello":"hello world"},
-    "status": "success"
-  }));
+app.get('/', (req, res) => {
+  res.send({
+    status: "success",
+    data: "hello world!"
+  })
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () => console.log('golie-api listening on port 3000!'));
